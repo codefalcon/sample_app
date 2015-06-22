d0d032818a25d3057c8b5153f1650e876fb4a238
Safiq M <safiq.m@freshdesk.com>
2015-06-22 19:47:38 +0530

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -40,11 +41,6 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
-  end
-
-  def signed_in_user
-    store_location
-    redirect_to signin_url, notice: "Please sign in." unless signed_in?
   end
 
   def correct_user
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
   #get "users/new"
   resources :users
   resources :sessions, only: [:new, :destroy, :create]
+  resources :microposts, only: [:create, :destroy]
   
   root to: 'static_pages#home'
   
